b704970e93af53f7a9515f7e00cd68dd7dc9db93
Juno <polya.alexei@yandex.ru>
2023-12-09 13:20:01 +0300

    added programms viewer

---
+++ src/main/java/Shelter.java
@@ -0,0 +1,217 @@
+import abstractAnimals.Animal;
+import animals.*;
+import databases.Database;
+import logs.Log;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+
+public class Shelter implements ShelterInterface {
+    private final View view;
+    private Log log;
+    private final Database db;
+
+    public Shelter(View view, Log log, Database db) {
+        this.log = log;
+        this.view = view;
+        this.db = db;
+    }
+
+    public void start() {
+        int choice = -1;
+        while (choice != 0) {
+            view.showMainMenu();
+            choice = view.inputNumber("Выберите пункт меню");
+
+            switch (choice) {
+                case 0:
+                    view.showMessage("Всего хорошего!");
+                    break;
+                case 1:
+                    view.showAnimalList(getAnimals());
+                    break;
+                case 2:
+                    String type = view.selectAnimalType();
+                    String name = view.inputString("Введите имя животного");
+                    int birthyear = view.inputYear("Введите год рождения животного");
+                    int birthmonth = view.inputMonth("Введите номер месяца рождения животного (1-12)");
+                    int birthday = view.inputDay("Введите день рождения животного");
+
+                    if (!checkDate(birthday, birthmonth, birthyear)) {
+                        view.showMessage("Такой даты не существует.");
+                        break;
+                    }
+
+                    addAnimal(createNewAnimal(type, name, birthyear, birthmonth, birthday));
+                    view.showAnimalList(getAnimals());
+                    break;
+                case 3:
+                    view.showAnimalList(getAnimals());
+
+                    int id = view.inputNumber("Введите ID животного для удаления (0 - отмена)");
+
+                    if (id == 0) break;
+
+                    Animal delAnimal = getAnimal(id);
+
+                    if (delAnimal != null) {
+                        delAnimal(id);
+                        view.showMessage("Удалено животное - " + delAnimal);
+                        view.showAnimalList(getAnimals());
+                    } else {
+                        view.showMessage("Не найдено животное с ID:" + id);
+                    }
+
+                    break;
+                case 4:
+                    view.showAnimalList(getAnimals());
+
+                    int curr_id = view.inputNumber("Введите ID животного для просмотра данных (0 - отмена)");
+                    if (curr_id == 0) break;
+
+                    Animal currentAnimal = getAnimal(curr_id);
+                    if (currentAnimal != null) {
+                        int choice2 = -1;
+                        while (choice2 != 0) {
+                            view.showAnimalParams(currentAnimal);
+                            view.showAnimalMenu();
+                            choice2 = view.inputNumber("Выберите пункт меню");
+
+                            switch (choice2) {
+                                case 1:
+                                    String skill = view.inputString("Введите новое умение");
+                                    currentAnimal.addAnimalSkill(skill);
+                                    updateAnimal(currentAnimal);
+                                    break;
+                                case 2:
+                                    view.showAnimalParams(currentAnimal);
+                                    int id_skill = view.inputNumber("Введите ID умения для удаления (0 - отмена)");
+
+                                    if (id_skill == 0) break;
+
+                                    currentAnimal.delAnimalSkill(id_skill - 1);
+
+                                    try {
+                                        db.updateAnimal(currentAnimal);
+                                    } catch (Exception e) {
+                                        log.append(e.getMessage());
+                                    }
+
+                                    view.showMessage("Умение было удалено.");
+
+                                    break;
+                            }
+                        }
+                    } else {
+                        view.showMessage("Не найдено животное с ID:" + curr_id);
+                    }
+                    break;
+            }
+        }
+    }
+
+    public ArrayList<Animal> getAnimals() {
+        try {
+            return db.getAnimalsList();
+        } catch (Exception e) {
+            log.append(e.getMessage());
+            view.showMessage(e.getMessage());
+        }
+
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void addAnimal(Animal animal) {
+        try {
+            db.addAnimal(animal);
+        } catch (Exception e) {
+            log.append(e.getMessage());
+            view.showMessage(e.getMessage());
+        }
+    }
+
+    @Override
+    public void delAnimal(int id) {
+        try {
+            db.delAnimal(id);
+        } catch (Exception e) {
+            log.append(e.getMessage());
+            view.showMessage(e.getMessage());
+        }
+    }
+
+    @Override
+    public Animal getAnimal(int id) {
+        Animal animal = null;
+        try {
+            animal = db.getAnimal(id);
+        } catch (Exception e) {
+            log.append(e.getMessage());
+            view.showMessage(e.getMessage());
+        }
+        return animal;
+    }
+
+    @Override
+    public void updateAnimal(Animal animal) {
+        try {
+            db.updateAnimal(animal);
+        } catch (Exception e) {
+            log.append(e.getMessage());
+            view.showMessage(e.getMessage());
+        }
+    }
+
+    private Animal createNewAnimal(String type, String name, int birthyear, int birthmonth, int birthday) {
+        Animal animal = null;
+
+        switch (type) {
+            case "Dog" -> {
+                animal = new Dog(name, birthday, birthmonth, birthyear);
+            }
+            case "Cat" -> {
+                animal = new Cat(name, birthday, birthmonth, birthyear);
+            }
+            case "Hamster" -> {
+                animal = new Hamster(name, birthday, birthmonth, birthyear);
+            }
+            case "Camel" -> {
+                animal = new Camel(name, birthday, birthmonth, birthyear);
+            }
+            case "Horse" -> {
+                animal = new Horse(name, birthday, birthmonth, birthyear);
+            }
+            case "Donkey" -> {
+                animal = new Donkey(name, birthday, birthmonth, birthyear);
+            }
+        }
+
+        return animal;
+    }
+
+    public static String formatDate(int day, int month, int year) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(year);
+        sb.append("-");
+        if (month < 10) sb.append("0");
+        sb.append(month);
+        sb.append("-");
+        if (day < 10) sb.append("0");
+        sb.append(day);
+
+        return sb.toString();
+    }
+
+    public static boolean checkDate(int day, int month, int year) {
+        try {
+            LocalDate date = LocalDate.parse(formatDate(day, month, year));
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/View.java
@@ -0,0 +1,189 @@
+import abstractAnimals.Animal;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.time.Year;
+
+public class View implements AutoCloseable {
+    public void showMainMenu() {
+        System.out.println("Главное меню:");
+        System.out.println("0.Выход");
+        System.out.println("1.Посмотреть список животных");
+        System.out.println("2.Добавить животное");
+        System.out.println("3.Удалить животное");
+        System.out.println("4.Открыть параметры животного (Команды)");
+    }
+
+    public void showAnimalMenu() {
+        System.out.println("Меню животного:");
+        System.out.println("0.Выход");
+        System.out.println("1.Добавить новое умение (команду)");
+        System.out.println("2.Удалить умение (команду)");
+    }
+
+    public void showMessage(String msg) {
+        System.out.println(msg);
+        System.out.println();
+    }
+
+    public void showAnimalTypes() {
+        System.out.println("Виды животных:");
+        System.out.println("1.Кошка");
+        System.out.println("2.Собака");
+        System.out.println("3.Хомяк");
+        System.out.println("4.Лошадь");
+        System.out.println("5.Осел");
+        System.out.println("6.Верблюд");
+    }
+
+    public String inputString(String query) {
+        while (true) {
+            System.out.print(query + ": ");
+            Scanner scanner = new Scanner(System.in);
+            String str = scanner.nextLine();
+            System.out.println();
+            str = str.replace(";", "").replace("|", "");
+
+            if (!str.equals("")) return str;
+        }
+    }
+
+    public int inputYear(String query) {
+        int year = 2000;
+        int currentYear = Year.now().getValue();
+
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            year = inputNumber(query);
+
+            if (year > currentYear) {
+                showMessage("Год больше текущего!");
+                flag = true;
+            }
+
+            if (currentYear - year > 100) {
+                showMessage("Некорректное число - попробуйте поменьше!");
+                flag = true;
+            }
+        }
+
+        return year;
+    }
+
+    public int inputMonth(String query) {
+        int month = 1;
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            month = inputNumber(query);
+
+            if (month < 1 || month > 12) {
+                showMessage("Месяц должен быть 1 - 12");
+                flag = true;
+            }
+        }
+
+        return month;
+    }
+
+    public int inputDay(String query) {
+        int day = 1;
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            day = inputNumber(query);
+
+            if (day < 1 || day > 31) {
+                showMessage("День должен быть 1 - 31");
+                flag = true;
+            }
+        }
+
+        return day;
+    }
+
+    public int inputNumber(String query) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print(query + ": ");
+            String str = scanner.nextLine();
+            System.out.println();
+
+            try {
+                return Integer.parseInt(str);
+            } catch (NumberFormatException ignored) {
+
+            }
+        }
+    }
+
+    public void showAnimalList(ArrayList<Animal> animals) {
+        if (animals == null || animals.size() == 0) {
+            System.out.println("В приюте нет ни одного животного.");
+            System.out.println();
+        } else {
+            System.out.println("Список животных в приюте:");
+            for (Animal animal : animals) {
+                if (animal != null) System.out.println(animal);
+            }
+            System.out.println();
+        }
+    }
+
+    public String selectAnimalType() {
+        String choice = "";
+        while (true) {
+            showAnimalTypes();
+            choice = inputString("Выберите тип животного" +
+                    "");
+            switch (choice) {
+                case "1":
+                    return "Cat";
+                case "2":
+                    return "Dog";
+                case "3":
+                    return "Hamster";
+                case "4":
+                    return "Horse";
+                case "5":
+                    return "Donkey";
+                case "6":
+                    return "Camel";
+            }
+        }
+    }
+
+    void showAnimalParams(Animal animal) {
+        System.out.println("Параметры: ");
+        System.out.println("id: " + animal.getId());
+        System.out.println("Животное: " + animal.getTypeName());
+        System.out.println("Имя: " + animal.getName());
+        System.out.println("Дата рождения: " + Shelter.formatDate(animal.getBirthday(), animal.getBirthmonth(), animal.getBirthyear()));
+        System.out.println("Возраст: " + Animal.calcAge(animal.getBirthday(), animal.getBirthmonth(), animal.getBirthyear()));
+        System.out.println();
+        showSkills(animal);
+    }
+
+    void showSkills(Animal animal) {
+        System.out.println("Навыки:");
+
+        if (animal.getSkills().size() == 0) {
+            System.out.println("Навыков еще нет.");
+        } else {
+            int i = 1;
+            for (String skill : animal.getSkills()) {
+                System.out.println("" + i + "." + skill);
+                i++;
+            }
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
